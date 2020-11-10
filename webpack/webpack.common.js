26f0bc87a7af5d4a026338c5878d005f7c2a4bc5
Rafael Lima <rafael.lima@pinchfinancial.com>
2020-11-10 09:42:01 -0500

    add Tamil as additional language

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -90,7 +90,8 @@ module.exports = (options) => ({
                 groupBy: [
                     { pattern: "./src/main/webapp/i18n/en/*.json", fileName: "./i18n/en.json" },
                     { pattern: "./src/main/webapp/i18n/zh-cn/*.json", fileName: "./i18n/zh-cn.json" },
-                    { pattern: "./src/main/webapp/i18n/pt-br/*.json", fileName: "./i18n/pt-br.json" }
+                    { pattern: "./src/main/webapp/i18n/pt-br/*.json", fileName: "./i18n/pt-br.json" },
+                    { pattern: "./src/main/webapp/i18n/ta/*.json", fileName: "./i18n/ta.json" }
                     // jhipster-needle-i18n-language-webpack - JHipster will add/remove languages in this array
                 ]
             }
--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -126,7 +126,8 @@ module.exports = webpackMerge(commonConfig({ env: ENV }), {
             localesToKeep: [
                     'en',
                     'zh-cn',
-                    'pt-br'
+                    'pt-br',
+                    'ta'
                     // jhipster-needle-i18n-language-moment-webpack - JHipster will add/remove languages in this array
                 ]
         }),
