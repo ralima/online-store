5e29cd8b963a38eb3794c9f91f71467cf946da55
Rafael Lima <rafael.lima@pinchfinancial.com>
2020-11-18 15:53:32 -0500

    update role based authorization logic

--- src/main/java/com/mycompany/store/repository/InvoiceRepository.java
+++ src/main/java/com/mycompany/store/repository/InvoiceRepository.java
@@ -2,8 +2,12 @@ package com.mycompany.store.repository;
 
 import com.mycompany.store.domain.Invoice;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 /**
  * Spring Data  repository for the Invoice entity.
@@ -11,4 +15,8 @@ import org.springframework.stereotype.Repository;
 @SuppressWarnings("unused")
 @Repository
 public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
+
+    Page<Invoice> findAllByOrderCustomerUserLogin(String login, Pageable pageable);
+
+    Optional<Invoice> findOneByIdAndOrderCustomerUserLogin(Long id, String login);
 }
--- src/main/java/com/mycompany/store/repository/ShipmentRepository.java
+++ src/main/java/com/mycompany/store/repository/ShipmentRepository.java
@@ -2,8 +2,12 @@ package com.mycompany.store.repository;
 
 import com.mycompany.store.domain.Shipment;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 /**
  * Spring Data  repository for the Shipment entity.
@@ -11,4 +15,7 @@ import org.springframework.stereotype.Repository;
 @SuppressWarnings("unused")
 @Repository
 public interface ShipmentRepository extends JpaRepository<Shipment, Long> {
+    Page<Shipment> findAllByInvoiceOrderCustomerUserLogin(String login, Pageable pageable);
+
+    Optional<Shipment> findOneByIdAndInvoiceOrderCustomerUserLogin(Long id, String login);
 }
